e3aa8b1c333468a4cc446f0e59f2d7dafd020340
nacopooh <nacopooh69@gmail.com>
2025-10-03 10:18:37 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('v1').then(c => c.addAll([
+      '/',
+      '/index.html',
+      '/manifest.webmanifest',
+      '/icon-180.png',
+      '/icon-512.png'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
